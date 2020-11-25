6d1ad1a4bbb1fb1706f30167c7b74146ba36037c
Tobiassa <tobiasgabriel23@hotmail.com>
2020-11-25 19:38:11 -0300

    adicionando a predefinição das seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Produto.create(nome: 'Luminária')
+Usuario.create(nome: 'Tobias')
+Cliente.create(nome: 'Sabrina')
